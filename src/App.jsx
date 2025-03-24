e5c2949cb0f8ed796004fd261efb03df597f7a5f
Oksana <ksenia.tkachenko.ua@gmail.com>
2025-03-23 20:18:32 -0400

    Initial commit: Starter Kit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '@styles/global.css';
+import '@styles/root.css';
+
+function App() {
+  return (
+    <div className="container"></div>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'url';
+
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@data': fileURLToPath(new URL('./src/data', import.meta.url)),
+      '@hooks': fileURLToPath(new URL('./src/hooks', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
+    },
+  },
+});
